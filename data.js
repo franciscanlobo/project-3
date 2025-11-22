f4b29f32a132826d29607e1ca015098c1d6df607
Kiko <franciscanlobo@gmail.com>
2025-11-22 18:03:12 +0800

    Initial Layout: Hero and Favorites Section

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [
+  {
+    firstName: "Bob",
+    description: "Description 2",
+    picture: "IMG_1782.JPG",
+  },
+  {
+    firstName: "Charlie",
+    description: "Description 3",
+    picture: "IMG_1413.JPG",
+  },
+  {
+    firstName: "David",
+    description: "Description 4",
+    picture: "IMG_1549.JPG",
+  },
+  {
+    firstName: "Eve",
+    description: "Description 5",
+    picture: "IMG_1347.jpg",
+  },
+];
